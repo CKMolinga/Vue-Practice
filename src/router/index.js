35c2a4cf6572428c4ea762987ad02ad1186556be
CKMolinga <ck.molinga@gmail.com>
2022-06-30 15:38:48 +0200

    Routing with CompAPI

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Details from '../views/Details.vue'
+import Create from '../views/Create.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
